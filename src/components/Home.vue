260248fe1629b7a3c786328ef412a0a574e7e520
Gonzalo <gonzaloaf7@gmail.com>
2020-02-05 09:57:52 -0400

    casi final en el landingpage o home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -26,7 +26,7 @@
           <v-flex xs12 sm4 class="my-4">
             <div class="text-center">
               <h2 class="headline">Adentrate al mundo de la música</h2>
-              <span class="subheading">En un solo lugar</span>
+              <span class="subheading">Playlist en un solo lugar</span>
             </div>
           </v-flex>
           <v-flex xs12>
@@ -40,9 +40,7 @@
                     <v-card-title primary-title class="layout justify-center">
                       <div class="headline text-center">¿Por qué Deezer?</div>
                     </v-card-title>
-                    <v-card-text>
-                      Encuentra tus futuras pasiones y tus canciones favoritas de siempre entre 56 millones de canciones.
-                    </v-card-text>
+                    <v-card-text>Encuentra tus futuras pasiones y tus canciones favoritas de siempre entre 56 millones de canciones.</v-card-text>
                   </v-card>
                 </v-flex>
                 <v-flex xs12 md4>
@@ -53,9 +51,7 @@
                     <v-card-title primary-title class="layout justify-center">
                       <div class="headline">Crea tu propia biblioteca musical</div>
                     </v-card-title>
-                    <v-card-text>
-                      Crea playlists con millones de canciones y llévalas contigo allá donde vayas.
-                      </v-card-text>
+                    <v-card-text>Crea playlists con millones de canciones y llévalas contigo allá donde vayas.</v-card-text>
                   </v-card>
                 </v-flex>
                 <v-flex xs12 md4>
@@ -66,9 +62,7 @@
                     <v-card-title primary-title class="layout justify-center">
                       <div class="headline text-center">¿No hay wifi? No pasa nada</div>
                     </v-card-title>
-                    <v-card-text>
-                      Con Deezer Premium, no necesitas estar conectado para disfrutar de tus canciones favoritas.
-                      </v-card-text>
+                    <v-card-text>Con Deezer Premium, no necesitas estar conectado para disfrutar de tus canciones favoritas.</v-card-text>
                   </v-card>
                 </v-flex>
               </v-layout>
@@ -84,73 +78,32 @@
           <v-layout column align-center justify-center>
             <div class="headline white--text mb-4 text-center">Mostrando el futuro de la música</div>
             <em>Sé parte del mundo de la innovación</em>
-            <v-btn class="mt-12" color="blue lighten-2" dark large>Descubre más</v-btn>
+            <v-btn
+              class="mt-12"
+              color="blue lighten-2"
+              dark
+              large
+              href="https://explore"
+            >Descubre más</v-btn>
           </v-layout>
         </v-parallax>
       </section>
 
-      <section>
-        <v-container grid-list-xl>
-          <v-layout row wrap justify-center class="my-12">
-            <v-flex xs12 sm4>
-              <v-card flat class="transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Company info</div>
-                </v-card-title>
-                <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                </v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex xs12 sm4 offset-sm1>
-              <v-card flat class="transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Contact us</div>
-                </v-card-title>
-                <v-card-text>Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.</v-card-text>
-                <v-list class="transparent">
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-phone</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>777-867-5309</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-map-marker</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>Chicago, US</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>john@vuetifyjs.com</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </section>
-
-      <v-footer color="blue darken-2">
+      <v-footer color="black darken-2">
         <v-layout row wrap align-center>
           <v-flex xs12>
             <div class="white--text ml-4">
-              Made with
-              <v-icon class="dark--text">mdi-heart</v-icon>by
-              <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-              and
-              <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+              Desarrollado por
+              <a
+                class="white--text"
+                href="https://github.com/penarrietatest"
+              >Ruben Peñarrieta</a> y
+              <a class="white--text" href="https://github.com/gonzaf7">Gonzalo Alfaro</a> usando
+              <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify.</a>
+              <a class="white--text">
+                Plantilla por
+                <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang.</a>
+              </a>
             </div>
           </v-flex>
         </v-layout>
