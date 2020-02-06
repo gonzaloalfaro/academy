f29e05e8576070c606ca0521c838c61c8db8a237
Ruben Penarrieta <penarrieta.test@gmail.com>
2020-02-05 21:42:26 -0400

    Initial commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <v-app dark>
-    <!-- <v-toolbar color="blue lighten-1">
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-    </v-toolbar>-->
     <v-content>
       <section>
         <v-parallax src="@/assets/Fondo.jpg" height="600">
           <v-layout column align-center justify-center class="white--text">
-            <img src="@/assets/logo.png" alt="Vuetify.js" height="200" />
+            <img src="@/assets/deezer1.png" alt="Vuetify.js" height="90" aspect-ratio="50%" />
+            <br />
             <h1 class="black--text mb-2 display-1 text-center">Las mejores playlist a tu alcance</h1>
             <div class="subheading black--text mb-4 text-center">Powered by Deezer</div>
-            <router-link to="/explore">
-              <v-btn class="mt-12" color="blue lighten-2" dark large>Get Started</v-btn>
-            </router-link>
+            <a href="/explore" target="_blank">
+              <v-btn
+                class="mt-12"
+                color="blue lighten-2"
+                dark
+                large
+              >Descubre más</v-btn>
+            </a>
           </v-layout>
         </v-parallax>
       </section>
@@ -21,8 +24,8 @@
         <v-layout column wrap class="my-12" align-center>
           <v-flex xs12 sm4 class="my-4">
             <div class="text-center">
-              <h2 class="headline">The best way to start developing</h2>
-              <span class="subheading">Cras facilisis mi vitae nunc</span>
+              <h2 class="headline">Adentrate al mundo de la música</h2>
+              <span class="subheading">Playlist en un solo lugar</span>
             </div>
           </v-flex>
           <v-flex xs12>
@@ -31,16 +34,12 @@
                 <v-flex xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2">mdi-palette</v-icon>
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-shopping-music</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-center">Material Design</div>
+                      <div class="headline text-center">¿Por qué Deezer?</div>
                     </v-card-title>
-                    <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                    </v-card-text>
+                    <v-card-text>Encuentra tus futuras pasiones y tus canciones favoritas de siempre entre 56 millones de canciones.</v-card-text>
                   </v-card>
                 </v-flex>
                 <v-flex xs12 md4>
@@ -49,28 +48,20 @@
                       <v-icon x-large class="blue--text text--lighten-2">mdi-flash</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline">Fast development</div>
+                      <div class="headline">Crea tu propia biblioteca musical</div>
                     </v-card-title>
-                    <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                    </v-card-text>
+                    <v-card-text>Crea playlists con millones de canciones y llévalas contigo allá donde vayas.</v-card-text>
                   </v-card>
                 </v-flex>
                 <v-flex xs12 md4>
                   <v-card flat class="transparent">
                     <v-card-text class="text-center">
-                      <v-icon x-large class="blue--text text--lighten-2">mdi-wrench</v-icon>
+                      <v-icon x-large class="blue--text text--lighten-2">mdi-headset</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-center">Completely Open Sourced</div>
+                      <div class="headline text-center">¿No hay wifi? No pasa nada</div>
                     </v-card-title>
-                    <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                    </v-card-text>
+                    <v-card-text>Con Deezer Premium, no necesitas estar conectado para disfrutar de tus canciones favoritas.</v-card-text>
                   </v-card>
                 </v-flex>
               </v-layout>
@@ -84,75 +75,34 @@
           src="https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-1.2.1&auto=format&fit=crop&w=1334&q=80"
         >
           <v-layout column align-center justify-center>
-            <div class="headline white--text mb-4 text-center">Desarrollo de una app</div>
-            <em>Kick-start your application today</em>
-            <v-btn class="mt-12" color="blue lighten-2" dark large>Get Started</v-btn>
+            <div class="headline white--text mb-4 text-center">Mostrando el futuro de la música</div>
+            <em>Sé parte del mundo de la innovación</em>
+            <v-btn
+              class="mt-12"
+              color="blue lighten-2"
+              dark
+              large
+              href="https://explore"
+            >Descubre más</v-btn>
           </v-layout>
         </v-parallax>
       </section>
 
-      <section>
-        <v-container grid-list-xl>
-          <v-layout row wrap justify-center class="my-12">
-            <v-flex xs12 sm4>
-              <v-card flat class="transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Company info</div>
-                </v-card-title>
-                <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                </v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex xs12 sm4 offset-sm1>
-              <v-card flat class="transparent">
-                <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Contact us</div>
-                </v-card-title>
-                <v-card-text>Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.</v-card-text>
-                <v-list class="transparent">
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-phone</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>777-867-5309</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-map-marker</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>Chicago, US</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                  <v-list-item>
-                    <v-list-item-action>
-                      <v-icon class="blue--text text--lighten-2">mdi-email</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                      <v-list-item-title>john@vuetifyjs.com</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </section>
-
-      <v-footer color="blue darken-2">
+      <v-footer color="black darken-2">
         <v-layout row wrap align-center>
           <v-flex xs12>
             <div class="white--text ml-4">
-              Made with
-              <v-icon class="red--text">mdi-heart</v-icon>by
-              <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-              and
-              <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
+              Desarrollado por
+              <a
+                class="white--text"
+                href="https://github.com/penarrietatest"
+              >Ruben Peñarrieta</a> y
+              <a class="white--text" href="https://github.com/gonzaf7">Gonzalo Alfaro</a> usando
+              <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify.</a>
+              <a class="white--text">
+                Plantilla por
+                <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang.</a>
+              </a>
             </div>
           </v-flex>
         </v-layout>
