fead296f57e1f386d2837d34fee271a2e23a5767
Ruben Penarrieta <penarrieta.test@gmail.com>
2020-02-05 22:15:34 -0400

    Initial commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -115,5 +115,7 @@
 export default {};
 </script>
 
-<style>
+<style lang="sass" scoped>
+  a
+    text-decoration: none
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -4,14 +4,13 @@
       <v-card class="mx-auto" raised>
         <v-list-item>
           <v-list-item-content>
-            <v-list-item-title class="headline mb-1"
-              >This is an about page</v-list-item-title
+            <v-list-item-title class="headline mb-1 text-center"
+              >App Deezer</v-list-item-title
             >
           </v-list-item-content>
         </v-list-item>
 
-        <pre> {{ info }} </pre>
-
+      <p class="pa-10">Desarrollado por Ruben Peñarrieta y Gonzalo Alfaro usando VueJS, consumiendo api de Deezer</p>
       </v-card>
     </v-container>
   </div>
